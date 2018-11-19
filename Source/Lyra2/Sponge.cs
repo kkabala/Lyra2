f2579d7d32554e80851b7282cd414094b2e07a35
Krzysztof Kabala <krzykab@gmail.com>
2018-11-19 21:23:13 +0100

    Add headers

--- Source/Lyra2/Sponge.cs
+++ Source/Lyra2/Sponge.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/***********************************************************************
+
+	Lyra2 .NET implementation written by Krzysztof Kabała <krzykab@gmail.com>.
+	It is distributed under MIT license.
+	It is based on C implementation written by The Lyra PHC Team, 
+	which can be found here: https://github.com/gincoin-dev/gincoin-core/blob/master/src/crypto/Lyra2Z/Lyra2.c 
+	Distribution of this file or code is only allowed with this header.
+
+
+/***********************************************************************/
+using System;
 using System.Linq;
 
 namespace Kabala.Lyra2
